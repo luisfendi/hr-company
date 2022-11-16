177924e274b17d5d650aa9b5d42897e56b73fba8
luisfendi@protonmail.com <luisfendi@protonmail.com>
2022-11-16 12:27:10 +0300

    стилизация блока options, анимация, оптимизация картинок

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import s from './Options.module.scss';
 import { optimize } from "../../js/optimizePhoto";
+import { Button } from '../button/Button';
+import { CSSTransition } from "react-transition-group";
+import './block_animate.scss';
 //photos
 import hRHub from '../../assets/img/hRHub.svg';
 import leave from '../../assets/img/leave.svg';
@@ -21,7 +24,6 @@ import business_time from '../../assets/img/business_mobile.jpg';
 
 export const Options = () => {
   const [theme, setTheme] = useState('hRHub')
-
   const map = {
     hRHub: [hRHub, '#EAF3FF', business_hRHub],
     mobile: [mobile, '#EAF3FF', business_mobile],
@@ -31,16 +33,19 @@ export const Options = () => {
     payroll: [payroll, '#F1F1FC', business_payroll],
     benefits: [benefits, '#F3E5F5', business_benefits],
   };
+  const [bild, setBild] = useState(true);
+  const nodeRef = useRef(null);
 
+  // UI часть компонента
   function Build() {
-    optimize(`.${s['bild-image']}`, 'optimize')
-    // optimize(`.image`, 'optimize')
+    useEffect(() => {
+      optimize(`.${s['bild-image']}`, 'optimize')
+    }, [])
     return (
       <>
         <h1>{theme}</h1>
-        <div 
-        className={`${s['bild-image']} image`}
-        // style={{backgroundImage: `url(${map[theme][2]})`}}  
+        <div
+          className={`${s['bild-image']} optimize`}
         >
           <img src={`${map[theme][2]}`} alt="" />
         </div>
@@ -49,15 +54,28 @@ export const Options = () => {
     )
   }
 
+
+  function set_animation_and_theme(propose){
+    if(propose == theme){
+      return
+    }
+    setBild(false);
+    setTimeout(() => {
+      setBild(true);
+      setTheme(propose);
+    }, 500)
+  }
+
+
   return (
     <div className={`${s.content} ${s.wrap} wrap`}>
-      <p>people operations platform</p>
-      <h2>Swingvy makes it easy to onboard, play, insure and support.</h2>
+      <p className={`${s['content-text']}`}>people operations platform</p>
+      <h2 className={`${s['content-title']}`}>Swingvy makes it easy to onboard, play, insure and support.</h2>
       <ul className={s.list}>
         {
           Object.keys(map).map((el, i) => (
             <li
-              onClick={() => { setTheme(el) }}
+              onClick={() => {  set_animation_and_theme(el)}}
               key={i}
               style={{ backgroundColor: `${map[el][1]}` }}>
               <span style={{ backgroundImage: `url(${map[el][0]})` }}></span>
@@ -66,10 +84,27 @@ export const Options = () => {
           ))
         }
       </ul>
-      <div
-        className={s.bild}
-        style={{ backgroundColor: `${map[theme][1]}` }}>
-        <Build />
+
+      <CSSTransition
+        in={bild}
+        timeout={1}
+        classNames="bild"
+        nodeRef={nodeRef}
+        appear={true}
+        mountOnEnter={true}
+      >
+        <div
+          ref={nodeRef}
+          className={s.bild}
+          style={{ backgroundColor: `${map[theme][1]}` }}>
+          <Build />
+        </div>
+      </CSSTransition>
+
+      
+      
+      <div className={s.button} onClick={() => { setBild(!bild) }}>
+        <Button >Learn more</Button>
       </div>
     </div>
   )
--- src/js/optimizePhoto.js
+++ src/js/optimizePhoto.js
@@ -1,13 +1,11 @@
 export const optimize = (selector, class_rem) => {
-  console.log(selector)
   const wrap = document.querySelector(selector);
   const img = wrap.querySelector('img');
 
+
   const new_img = new Image();
-  new_img.src = img.dataset.src;
-  
+  new_img.src = img.getAttribute('src');
   new_img.onload = () => {
-    console.log('end')
-    img.classList.remove(class_rem);
+    wrap.classList.remove(class_rem);
   }
 }
