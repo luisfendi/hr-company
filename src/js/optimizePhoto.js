9f9b6e49cab5eb9432b211013f2dc74978af4d4c
juicyvveb <luceriy87@mail.ru>
2022-11-20 10:28:35 +0300

    подправлена оптимизация фото

--- src/js/optimizePhoto.js
+++ src/js/optimizePhoto.js
@@ -1,11 +1,12 @@
 export const optimize = (selector, class_rem) => {
   const wrap = document.querySelectorAll(selector);
-  // console.log(wrap)
+  console.log(wrap)
   const img = wrap[0].querySelector('img');
   const new_img = new Image();
   new_img.src = img.getAttribute('src');
   new_img.onload = () => {
     console.log(wrap)
     wrap.forEach(el => el.classList.remove(class_rem))
+    console.log('end')
   }
 }
