bbb0f57aad50f67fc64fae58a077ac363efe1c58
juicyvveb <luceriy87@mail.ru>
2022-11-20 14:17:29 +0300

    сделаны выпадающие списки в хедере

---
+++ src/js/dropMenu.js
@@ -0,0 +1,19 @@
+export const drop_menu = (elem_selector, toggle_selector) => {
+    const drop_button = document.querySelectorAll(`.${elem_selector}`)
+    drop_button.forEach((el) => {
+        el.onclick = function (e) {
+            const current = this;
+            const close = () => {
+                current.classList.remove(toggle_selector);
+                document.removeEventListener('click', close)
+            }
+
+            e.preventDefault();
+            [...drop_button]
+                .filter(el => el.classList.contains(toggle_selector) && el != this)
+                .forEach(el => el.classList.remove(toggle_selector))
+            this.classList.toggle(toggle_selector);
+            document.addEventListener('click', close)
+        }
+    })
+}
--- src/js/optimizePhoto.js
+++ src/js/optimizePhoto.js
@@ -1,12 +1,9 @@
 export const optimize = (selector, class_rem) => {
   const wrap = document.querySelectorAll(selector);
-  console.log(wrap)
   const img = wrap[0].querySelector('img');
   const new_img = new Image();
   new_img.src = img.getAttribute('src');
   new_img.onload = () => {
-    console.log(wrap)
     wrap.forEach(el => el.classList.remove(class_rem))
-    console.log('end')
   }
 }
